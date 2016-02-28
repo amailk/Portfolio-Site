1a3be089dafb386671b68ce366aa18e4bbfde2cd
Ama Liyanage <liyaniama@gmail.com>
2016-02-28 11:42:19 -0500

    cleaned code

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -3,6 +3,6 @@ $(document).ready(function () {
     $("#header").hover(function () {
         $("#header").attr("src", "images/header-color.jpg");
     }, function () {
-        $("#header").attr("src", "images/header-bw.jpg");
+        $("#hesader").attr("src", "images/header-bw.jpg");
     });
 });
