b600ff32b6a2acbd24aed40dc72d6fc5ac2d0b06
Ama Liyanage <liyaniama@gmail.com>
2016-03-03 13:21:35 -0500

    added new project.

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -3,6 +3,6 @@ $(document).ready(function () {
     $("#header").hover(function () {
         $("#header").attr("src", "images/header-color.jpg");
     }, function () {
-        $("#hesader").attr("src", "images/header-bw.jpg");
+        $("#header").attr("src", "images/header-bw.jpg");
     });
 });
